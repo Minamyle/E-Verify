960c11143657168887335f0bcb19321b46313c09
Amina Ismaila <minamyle@gmail.com>
2024-05-31 00:50:28 +0100

    added: more features

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,14 @@
-import Footer from "../components/Footer"
-import Navbar from "../components/Navbar"
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
-const Layout = ({children}: any) => {
+const Layout = ({ children }: any) => {
   return (
     <div className="w-[100vw] overflow-x-hidden">
-        <Navbar />
-        {children}
-        {/* <Footer /> */}
+      <Navbar />
+      {children}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
